b62a731ab542942905c727ec0e43c1fcfbbbd650
lin3598197 <lin3598197@gmail.com>
2026-07-12 19:15:34 +0800

    remove sh files

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env/ bash
-
-git clone https://github.com/lin3598197/gnex_cli.git
-chmod +x run.sh
-source run.sh
-
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt
-.venv/bin/python app.py
