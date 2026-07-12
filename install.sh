609b2759fd8a33f614130487a7b19a73481dbdfa
lin3598197 <lin3598197@gmail.com>
2026-07-12 18:40:31 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env/ bash
 
 git clone https://github.com/lin3598197/gnex_cli.git
-chmod +x run.sh
-./run.sh
-
+cd gnex_cli
+python3 -m venv .venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 app.py
